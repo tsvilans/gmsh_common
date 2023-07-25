4799e19fda8cf33cd254fc80734eccd1484eb702
Tom Svilans <tom.svilans@gmail.com>
2023-07-25 09:41:47 +0200

    feat(init): first commit

---
+++ src/Gmsh.GH/Cmpt_Mesh2D.cs
@@ -0,0 +1,163 @@
+﻿
+/*
+ * Gmsh.GH
+ * Copyright 2023 Tom Svilans
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Rhino;
+using Rhino.Geometry;
+using Grasshopper.Kernel;
+using Grasshopper.Kernel.Data;
+using Grasshopper.Kernel.Types;
+using Grasshopper;
+
+using GmshCommon;
+
+namespace GmshCommon.GH
+{
+
+    public class Cmpt_Mesh2D : GH_Component
+    {
+        public Cmpt_Mesh2D()
+            : base("Gmsh2D", "Gmsh2D",
+                "Mesh 2D entities.",
+                "Gmsh", "Meshing")
+        {
+        }
+
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+            pManager.AddGenericParameter("Mesh", "M", "Mesh to remesh.", GH_ParamAccess.item);
+            pManager.AddNumberParameter("SizeMin", "Min", "Minimum mesh size.", GH_ParamAccess.item, 10.0);
+            pManager.AddNumberParameter("SizeMax", "Max", "Maximum mesh size.", GH_ParamAccess.item, 100.0);
+            pManager.AddBooleanParameter("Remesh", "R", "Remesh mesh geometry.", GH_ParamAccess.item, false);
+
+            pManager[1].Optional = true;
+            pManager[2].Optional = true;
+            pManager[3].Optional = true;
+        }
+
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+            pManager.AddMeshParameter("Mesh", "M", "Meshed object.", GH_ParamAccess.list);
+
+            pManager.AddIntegerParameter("Node IDs", "NI", "Node IDs.", GH_ParamAccess.list);
+            pManager.AddPointParameter("Node Positions", "NP", "Node positions.", GH_ParamAccess.list);
+            pManager.AddIntegerParameter("Element IDs", "EI", "Element IDs.", GH_ParamAccess.list);
+            pManager.AddIntegerParameter("Element Nodes", "EN", "Element node IDs.", GH_ParamAccess.tree);
+        }
+
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            double size_min = 10.0, size_max = 100.0;
+            bool create_geometry = false;
+
+            DA.GetData("SizeMin", ref size_min);
+            DA.GetData("SizeMax", ref size_max);
+            DA.GetData("Remesh", ref create_geometry);
+
+            Mesh mesh = null;
+
+            DA.GetData("Mesh", ref mesh);
+
+            if (mesh == null) return;
+
+            Gmsh.InitializeGmsh();
+            Gmsh.Clear();
+            Gmsh.Logger.Start();
+
+            var mesh_id = -1;
+
+            // Add mesh data
+            try
+            {
+                mesh_id = GmshCommon.GeometryExtensions.TransferMesh(mesh, create_geometry);
+            }
+            catch (Exception e)
+            {
+                string msg = Gmsh.Logger.GetLastError();
+
+                var log = Gmsh.Logger.Get();
+                foreach (string l in log)
+                    msg += String.Format("\n    {0}", log);
+
+                throw new Exception(msg);
+            }
+
+            if (mesh_id < 0) return;
+
+
+            // Get 2D entities (the mesh we just transferred)
+             Tuple<int, int>[] surfaceTags;
+             Gmsh.GetEntities(out surfaceTags, 2);
+            //Message = String.Format("{0} : {1}", mesh_id, surfaceTags[0].Item2);
+
+            var loop = Gmsh.Geo.AddSurfaceLoop(surfaceTags.Select(x => x.Item2).ToArray());
+            
+            //var loop = Gmsh.Geo.AddSurfaceLoop(new int[] { mesh_id });
+            var vol = Gmsh.Geo.AddVolume(new int[] { loop });
+
+            Gmsh.Geo.Synchronize();
+
+            // Set mesh sizes
+            int element_order = 1;
+            Gmsh.SetNumber("Mesh.MeshSizeMin", size_min);
+            Gmsh.SetNumber("Mesh.MeshSizeMax", size_max);
+
+            Gmsh.SetNumber("Mesh.SaveAll", 0);
+            Gmsh.SetNumber("Mesh.SaveGroupsOfElements", -1001);
+            Gmsh.SetNumber("Mesh.SaveGroupsOfNodes", 2);
+            Gmsh.SetNumber("Mesh.ElementOrder", element_order);
+
+            // Generate mesh
+            Gmsh.Generate(3);
+
+            mesh = GmshCommon.GeometryExtensions.GetMesh();
+
+            mesh.Compact();
+            List<Mesh> meshes = new List<Mesh>();
+
+            meshes.Add(mesh);
+
+            DA.SetDataList(0, meshes);
+
+            Gmsh.FinalizeGmsh();
+        }
+
+        protected override System.Drawing.Bitmap Icon
+        {
+            get
+            {
+                return null;
+                //  return Properties.Resources.GridSample_01;
+            }
+        }
+
+        public override Guid ComponentGuid
+        {
+            /*
+                0fe48fc4-68ff-484a-949c-37bae7f882c1
+                45094c4f-ef59-4a0a-b30a-6a3fcc21a024
+             */
+            get { return new Guid("242113a3-bccd-476b-89b1-306c18d4e930"); }
+        }
+    }
+}
---
+++ src/Gmsh.GH/Utility.cs
@@ -0,0 +1,364 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Rhino.Geometry;
+using Grasshopper.Kernel.Types;
+
+using Pair = System.Tuple<int, int>;
+
+namespace GmshCommon
+{
+    public static class GeometryExtensions
+    {
+        /// <summary>
+        /// Returns a Rhino mesh from Gmsh 2D elements. Gmsh must be initialized beforehand.
+        /// </summary>
+        /// <returns>A mesh.</returns>
+        public static Mesh GetMesh(int entity = -1)
+        {
+            // Get a mesh out
+            IntPtr[] nodeTags;
+            double[] coords;
+            Gmsh.Mesh.GetNodes(out nodeTags, out coords, 3, -1, true, false);
+
+            if (nodeTags == null || nodeTags.Length < 1) throw new Exception("Bad nodeTags in GetMesh()");
+
+            var nodes = new Point3d[nodeTags.Length + 1];
+
+            for (int i = 0; i < nodeTags.Length; ++i)
+            {
+                var pt = new Point3d(coords[i * 3], coords[i * 3 + 1], coords[i * 3 + 2]);
+                nodes[(int)nodeTags[i]] = pt;
+            }
+
+            int[] elementTypes;
+            IntPtr[][] elementTags, enodeTags;
+
+            
+            Gmsh.Mesh.GetElements(out elementTypes, out elementTags, out enodeTags, 2, -1);
+
+            var mesh = new Mesh();
+
+            mesh.Vertices.AddVertices(nodes);
+
+            for (int i = 0; i < elementTypes.Length; ++i)
+            {
+                if (elementTypes[i] == 2)
+                {
+
+                    for (int j = 0; j < enodeTags[i].Length; j += 3)
+                    {
+                        mesh.Faces.AddFace(
+                          (int)enodeTags[0][j],
+                          (int)enodeTags[0][j + 1],
+                          (int)enodeTags[0][j + 2]);
+                    }
+                }
+                else if (elementTypes[i] == 3)
+                {
+
+                    for (int j = 0; j < enodeTags[i].Length; j += 4)
+                    {
+                        mesh.Faces.AddFace(
+                          (int)enodeTags[0][j],
+                          (int)enodeTags[0][j + 1],
+                          (int)enodeTags[0][j + 2],
+                          (int)enodeTags[0][j + 3]);
+                    }
+                }
+                else
+                {
+                    continue;
+                }
+            }
+
+            mesh.Compact();
+            mesh.RebuildNormals();
+            mesh.Unweld(0.1, true);
+
+            return mesh;
+        }
+
+        /// <summary>
+        /// Get mesh for specific physical group.
+        /// </summary>
+        /// <param name="dim">Dimensions of physical group.</param>
+        /// <param name="tag">Tag of physical group.</param>
+        /// <returns></returns>
+        public static Mesh GetMeshForPhysicalGroup(int dim, int tag)
+        {
+            IntPtr[] nodeTags;
+            double[] coords;
+            Gmsh.Mesh.GetNodes(out nodeTags, out coords, 3, -1, true, false);
+
+            var nodes = new Point3d[nodeTags.Length + 1];
+
+            for (int i = 0; i < nodeTags.Length; ++i)
+            {
+                var pt = new Point3d(coords[i * 3], coords[i * 3 + 1], coords[i * 3 + 2]);
+                nodes[(int)nodeTags[i]] = pt;
+            }
+
+            int[] elementTypes;
+            IntPtr[][] elementTags, enodeTags;
+
+            var entities = Gmsh.GetEntitiesForPhysicalGroup(dim, tag);
+
+            var mesh = new Mesh();
+            mesh.Vertices.AddVertices(nodes);
+
+            foreach (int ent in entities)
+            {
+                Gmsh.Mesh.GetElements(out elementTypes, out elementTags, out enodeTags, dim, ent);
+
+                for (int i = 0; i < elementTypes.Length; ++i)
+                {
+                    if (elementTypes[i] == 2)
+                    {
+
+                        for (int j = 0; j < enodeTags[i].Length; j += 3)
+                        {
+                            mesh.Faces.AddFace(
+                              (int)enodeTags[0][j],
+                              (int)enodeTags[0][j + 1],
+                              (int)enodeTags[0][j + 2]);
+                        }
+                    }
+                    else if (elementTypes[i] == 3)
+                    {
+
+                        for (int j = 0; j < enodeTags[i].Length; j += 4)
+                        {
+                            mesh.Faces.AddFace(
+                              (int)enodeTags[0][j],
+                              (int)enodeTags[0][j + 1],
+                              (int)enodeTags[0][j + 2],
+                              (int)enodeTags[0][j + 3]);
+                        }
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+            }
+
+            mesh.Compact();
+            mesh.RebuildNormals();
+            mesh.Unweld(0.1, true);
+
+            return mesh;
+        }
+
+        /// <summary>
+        /// Transfers a Rhino mesh to Gmsh as a 2D surface entity. 
+        /// </summary>
+        /// <param name="mesh">Surface mesh to be volumetrically meshed.</param>
+        /// <param name="create_geometry">Sometimes the reconstruction of geometry fails, in which case this settings needs to be toggled.</param>
+        public static int TransferMesh(Mesh mesh, bool create_geometry = false)
+        {
+            var entity = Gmsh.AddDiscreteEntity(2, -1);
+
+            mesh = mesh.DuplicateMesh();
+            mesh.Weld(Math.PI);
+
+            mesh.Faces.ConvertQuadsToTriangles();
+            mesh.RebuildNormals();
+            mesh.Compact();
+
+            double[] coords = new double[mesh.Vertices.Count * 3];
+            var tags = new IntPtr[mesh.Vertices.Count];
+            for (int i = 0; i < tags.Length; ++i)
+                tags[i] = (IntPtr)i + 1;
+
+            for (int i = 0; i < mesh.Vertices.Count; ++i)
+            {
+                coords[i * 3 + 0] = mesh.Vertices[i].X;
+                coords[i * 3 + 1] = mesh.Vertices[i].Y;
+                coords[i * 3 + 2] = mesh.Vertices[i].Z;
+            }
+
+            Gmsh.Mesh.AddNodes(2, entity, tags, coords);
+
+            var faces3 = new IntPtr[mesh.Faces.TriangleCount];
+            var faces4 = new IntPtr[mesh.Faces.QuadCount];
+
+            var faceindices3 = new IntPtr[mesh.Faces.TriangleCount * 3];
+            var faceindices4 = new IntPtr[mesh.Faces.QuadCount * 4];
+
+            int i3 = 0, i4 = 0;
+
+            for (int i = 0; i < mesh.Faces.Count; ++i)
+            {
+                var mf = mesh.Faces[i];
+
+                if (mf.IsQuad)
+                {
+                    faces4[i4] = (IntPtr)i + 1;
+                    faceindices4[i4 * 4] = (IntPtr)mf.A + 1;
+                    faceindices4[i4 * 4 + 1] = (IntPtr)mf.B + 1;
+                    faceindices4[i4 * 4 + 2] = (IntPtr)mf.C + 1;
+                    faceindices4[i4 * 4 + 3] = (IntPtr)mf.D + 1;
+                    i4++;
+                }
+                else
+                {
+                    faces3[i3] = (IntPtr)i + 1;
+                    faceindices3[i3 * 3] = (IntPtr)mf.A + 1;
+                    faceindices3[i3 * 3 + 1] = (IntPtr)mf.B + 1;
+                    faceindices3[i3 * 3 + 2] = (IntPtr)mf.C + 1;
+                    i3++;
+                }
+            }
+
+            var elementTypes = new List<int>();
+            var elementTags = new List<IntPtr[]>();
+            var nodeTags = new List<IntPtr[]>();
+            if (faces3.Length > 0)
+            {
+                elementTypes.Add(2);
+                elementTags.Add(faces3);
+                nodeTags.Add(faceindices3);
+            }
+
+            if (faces4.Length > 0)
+            {
+                elementTypes.Add(3);
+                elementTags.Add(faces4);
+                nodeTags.Add(faceindices4);
+            }
+
+            Gmsh.Mesh.AddElements(2, entity, elementTypes.ToArray(),
+              elementTags.ToArray(),
+              nodeTags.ToArray());
+
+            Gmsh.Mesh.CreateTopology(true, true);
+            Gmsh.Mesh.ClassifySurfaces(0.1, true, true, 0.1, true);
+
+            if (create_geometry)
+                Gmsh.Mesh.CreateGeometry();
+
+            return entity;
+        }
+
+        public static List<Point3d> GetCentroids()
+        {
+            var centroids = new List<Point3d>();
+            try
+            {
+                int[] elementTypes;
+                IntPtr[][] elementTags, enodeTags;
+                Gmsh.Mesh.GetElements(out elementTypes, out elementTags, out enodeTags, 3, -1);
+
+                for (int i = 0; i < elementTypes.Length; ++i)
+                {
+                    for (int j = 0; j < elementTags[i].Length; ++j)
+                    {
+                        var centroid = GmshCommon.Utility.GetCentroid(elementTags[i][j], elementTypes[i]);
+                        centroids.Add(new Point3d(centroid[0], centroid[1], centroid[2]));
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                string msg = Gmsh.Logger.GetLastError();
+
+                var log = Gmsh.Logger.Get();
+                foreach (string l in log)
+                    msg += String.Format("\n{0}", log);
+
+                throw new Exception(msg);
+            }
+
+            return centroids;
+        }
+
+        /// <summary>
+        /// Fragment the model and return the map of children entities.
+        /// </summary>
+        /// <param name="dimTags">Array of (dim, tag) pairs to include in the fragmentation.</param>
+        /// <returns>A dictionary that maps the input entities with the new or modified output entities.</returns>
+        public static Dictionary<Pair, List<Pair>> Fragment(Pair[] dimTags, out Pair[] dimTagsOut, out Pair[][] dimTagsMap)
+        {
+            if (dimTags.Length < 2)
+            {
+                dimTagsOut = new Pair[0];
+                dimTagsMap = new Pair[0][];
+                return null;
+            }
+
+            Array.Sort(dimTags); // Important for putting the lower dimension entities before higher ones
+
+            Gmsh.SetNumber("Geometry.OCCBooleanPreserveNumbering", 1);
+
+            var obj = new Pair[] { dimTags[0] };
+            var tools = new Pair[dimTags.Length - 1];
+            Array.Copy(dimTags, 1, tools, 0, tools.Length);
+
+            Gmsh.OCC.Fragment(obj, tools, out dimTagsOut, out dimTagsMap, true, true);
+            Gmsh.OCC.Synchronize();
+
+
+            var all = new Tuple<int, int>[obj.Length + tools.Length];
+            Array.Copy(obj, 0, all, 0, obj.Length);
+            Array.Copy(tools, 0, all, obj.Length, tools.Length);
+
+            var Map = new Dictionary<Pair, List<Pair>>();
+
+            for (int i = 0; i < all.Length; ++i)
+            {
+                Map[all[i]] = new List<Pair>();
+                if (dimTagsMap[i].Length > 0)
+                {
+                    Map[all[i]].AddRange(dimTagsMap[i]);
+                }
+                else
+                {
+                    Map[all[i]].Add(new Pair(1, 1));
+                    //Map[all[i]].Add(all[i]);
+                }
+            }
+
+            return Map;
+        }
+
+
+    }
+
+    public static class Helpers
+    {
+        public static Dictionary<U, List<T>> Invert<T, U>(Dictionary<T, List<U>> dict)
+        {
+            var inv = new Dictionary<U, List<T>>();
+            foreach (var kvp in dict)
+            {
+                foreach (var i in kvp.Value)
+                {
+                    if (!inv.ContainsKey(i))
+                        inv[i] = new List<T>();
+                    inv[i].Add(kvp.Key);
+                }
+            }
+
+            return inv;
+        }
+
+        public static string DictToString<T, U>(Dictionary<T, List<U>> dict)
+        {
+            var list = new List<string>();
+            foreach (var kvp in dict)
+            {
+                string line = string.Format("{0} --> ", kvp.Key);
+                line += string.Join(", ", kvp.Value);
+
+                list.Add(line);
+            }
+
+            return string.Join("\n", list);
+        }
+
+    }
+}
